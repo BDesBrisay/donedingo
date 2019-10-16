4c21bc7fed6f54ad6e7fe96a4144fd8f7c404626
Bryce DesBrisay <bryce@desbrisay.com>
2019-10-15 19:13:10 -0600

    basic style for landing page

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -5,12 +5,15 @@ import styles from './Landing.module.css';
 
 const Landing = () => (
   <div className={styles.contain}>
-    LANDING PAGE
-    <Link to="/Dash">
-      <button>
-        SIGN IN
-      </button>
-    </Link>
+    <header className={styles.header}>
+      <h1 className={styles.logo}>Donedingo</h1>
+      <p className={styles.tagline}>Project Tracking Simplified.</p>
+      <Link to="/Dash">
+        <button>
+          SIGN IN
+        </button>
+      </Link>
+    </header>
   </div>
 );
 
