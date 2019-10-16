9daa7d84b6f9b3be889a7d0d408e03e960d5e562
Bryce DesBrisay <bryce@desbrisay.com>
2019-10-15 19:24:22 -0600

    layed out three column dashboard, styled frame

--- src/Components/Dash/Dash.js
+++ src/Components/Dash/Dash.js
@@ -5,12 +5,25 @@ import styles from './Dash.module.css';
 
 const Dash = () => (
   <div className={styles.contain}>
-    DASHBOARD
-    <Link to="/">
-      <button>
-        SIGN OUT
-      </button>
-    </Link>
+    <header className={styles.header}>
+      <h3>Donedingo</h3>
+      <Link to="/">
+        <button>
+          SIGN OUT
+        </button>
+      </Link>
+    </header>
+    <div className={styles.grid}>
+      <div className={styles.col}>
+        <h4>Objectives</h4>
+      </div>
+      <div className={styles.col}>
+        <h4>Key Results</h4>
+      </div>
+      <div className={styles.col}>
+        <h4>Tasks</h4>
+      </div>
+    </div>
   </div>
 )
 
