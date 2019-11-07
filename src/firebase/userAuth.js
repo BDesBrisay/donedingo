82a3f3d8f8822e69c8f0111f31274cb7ee4acd59
BDesBrisay <bryce@desbrisay.com>
2019-11-07 16:05:57 -0700

    commented userauth

--- src/firebase/userAuth.js
+++ src/firebase/userAuth.js
@@ -2,21 +2,28 @@ import { setUser } from "../utils/userState";
 
 async function userAuth({ users, user = {} }) {
   try {
+    // setup user structure
     const body = {
       id: user.uid,
       name: user.displayName,
       profileImage: user.photoURL,
       email: user.email
     };
+
+    // get doc reference
     const doc = users.doc(body.id);
     const get = await doc.get();
 
+    // check if user exists
     if (get.exists) {
       const data = await get.data();
+      // set user into local storage
       setUser(data);
       return data;
     }
     else {
+      // create new user
+      body.createdAt = new Date();
       await doc.set(body);
       return body;
     }
